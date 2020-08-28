a4df143e8d90a4a1aab7e9c7a586c4872ee1d7e3
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-08-28 22:36:40 +0200

    Add state information to Item
    This commit will add state information to Item.

--- mavenproject2/src/main/java/no/maardal/fant/Item.java
+++ mavenproject2/src/main/java/no/maardal/fant/Item.java
@@ -11,4 +11,64 @@ package no.maardal.fant;
  */
 public class Item {
     
+    private long id;
+    
+    private String title;
+    private String description;
+    private String category;
+
+    private User seller;
+
+    public Item() {
+    }
+
+    public Item(long id, String title, String description, String category, User seller) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.seller = seller;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+    
+    
 }
