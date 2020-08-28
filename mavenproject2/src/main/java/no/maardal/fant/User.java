7a4694ec57c2f9943d81674e94ded3565a4a47ec
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-08-28 22:25:01 +0200

    Added state information to User
    Added first name, last name and role to User.

--- mavenproject2/src/main/java/no/maardal/fant/User.java
+++ mavenproject2/src/main/java/no/maardal/fant/User.java
@@ -11,4 +11,34 @@ package no.maardal.fant;
  */
 public class User {
     
+    private String firstName;
+    private String lastName;
+    private String role;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    
 }
