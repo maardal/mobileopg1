e25d3adaf652e49faf508ac433d3ecd2fc087a10
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-09-01 14:41:37 +0200

    Annotated Photo and User

--- mavenproject2/src/main/java/no/maardal/fant/entities/User.java
+++ mavenproject2/src/main/java/no/maardal/fant/entities/User.java
@@ -1,53 +1,100 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package no.maardal.fant;
+package no.maardal.fant.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Represents an user in the Fant webstore
  * @author Martin
  */
-public class User {
+
+@Entity
+@Table(name = "AUSER")
+public class User implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userid")
+    private long userId;
+    
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    
+    @Column(name = "user_password", nullable = false)
+    private String password;
+    
+    @Column(name = "firstName", nullable = false)
     private String firstName;
+    
+    @Column(name = "lastname", nullable = false)
     private String lastName;
-    private String role;
+    
+    @Column(name = "user_group", nullable = false)
+    private String group;
 
     public User() {
     }
-    
-    public User(String firstName, String lastName, String role) {
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.role = role;
-    }
-    
-    public String getFirstName() {
-        return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getGroup() {
+        return group;
     }
 
-    public String getRole() {
-        return role;
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (int) (this.userId ^ (this.userId >>> 32));
+        return hash;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (this.userId != other.userId) {
+            return false;
+        }
+        return true;
     }
-    
     
     
 }
