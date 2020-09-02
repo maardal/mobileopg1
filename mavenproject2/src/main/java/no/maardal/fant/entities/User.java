a29a630f813fd38a5e07f57fff3659833e0b143a
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-09-02 17:56:32 +0200

    Add Bean Validation and required setters to User

--- mavenproject2/src/main/java/no/maardal/fant/entities/User.java
+++ mavenproject2/src/main/java/no/maardal/fant/entities/User.java
@@ -1,12 +1,16 @@
 package no.maardal.fant.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 /**
  * Represents an user in the Fant webstore
@@ -21,16 +25,22 @@ public class User implements Serializable{
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "userid")
     private long userId;
-    
+   
+    @Email
+    @NotNull
     @Column(name = "email", nullable = false, unique = true)
     private String email;
     
+    
+    @NotNull
     @Column(name = "user_password", nullable = false)
     private String password;
     
+    @NotNull
     @Column(name = "firstName", nullable = false)
     private String firstName;
     
+    @NotNull
     @Column(name = "lastname", nullable = false)
     private String lastName;
     
@@ -70,6 +80,23 @@ public class User implements Serializable{
     public String getGroup() {
         return group;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
 
     @Override
     public int hashCode() {
