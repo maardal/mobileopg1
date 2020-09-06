08632adeb42176f2bcc9d6f461ada244aa6fefb8
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-09-06 14:26:26 +0200

    Add SecurityConfiguration to define all IdentityStores, allowed roles
    This commit will
    add a security configuration class that defines all identiystores, allowed roles for login, and method for login (JWT)

---
+++ mavenproject2/src/main/java/no/maardal/fant/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package no.maardal.fant;
+
+import javax.annotation.security.DeclareRoles;
+import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import javax.security.enterprise.identitystore.PasswordHash;
+import no.maardal.fant.auth.Group;
+import org.eclipse.microprofile.auth.LoginConfig;
+
+@DatabaseIdentityStoreDefinition(
+    dataSourceLookup = DatasourceProducer.JNDI_NAME,
+    callerQuery = "select password from auser where userid = ?",
+    groupsQuery = "select name from ausergroup where userid = ?",
+    hashAlgorithm = PasswordHash.class,
+    priority = 80)
+@DeclareRoles({Group.ADMIN,Group.USER})
+@LoginConfig(authMethod = "MP-JWT", realmName = "template")
+public class SecurityConfiguration {
+    
+}
