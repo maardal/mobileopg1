9bc3a5765b6ac655372a1fb92e61a0e448e5d4a1
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-09-06 14:40:46 +0200

    Add run on startup class
    This commit will
    add a class that runs on startup, that creates user groups that is used in the project, if not already in database.

---
+++ mavenproject2/src/main/java/no/maardal/fant/auth/AuthRunOnStartup.java
@@ -0,0 +1,23 @@
+package no.maardal.fant.auth;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Singleton
+@Startup
+public class AuthRunOnStartup {
+    @PersistenceContext
+    EntityManager em;
+    
+    @PostConstruct
+    public void init() {
+        long groups = (long) em.createQuery("SELECT count (g.name) from Group g").getSingleResult();
+        if (groups == 0) {
+            em.persist(new Group(Group.USER));
+            em.persist(new Group(Group.ADMIN));
+        }
+    }
+}
